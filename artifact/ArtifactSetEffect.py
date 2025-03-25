f70e039602c373b4d9eaf1c54d539a5b97055570
MCZME <126310378+MCZME@users.noreply.github.com>
2025-03-26 02:13:51 +0800

    优化烬城勇者绘卷效果及代码结构

--- artifact/ArtifactSetEffect.py
+++ artifact/ArtifactSetEffect.py
@@ -1,7 +1,8 @@
 from character.character import Character
-from setup.BaseEffect import CinderCityEffect
+from setup.BaseEffect import  ElementalDamageBoostEffect
 from setup.DamageCalculation import DamageType
 from setup.Event import EnergyChargeEvent, EventBus, EventHandler, EventType
+from setup.Team import Team
 from setup.Tool import GetCurrentTime
 
 class ArtifactEffect(EventHandler):
@@ -50,6 +51,80 @@ class ObsidianCodex(ArtifactEffect):
         elif event.event_type == EventType.AFTER_NIGHTSOUL_BLESSING:
             attributePanel = event.data['character'].attributePanel
             attributePanel['伤害加成'] -= 15
+
+class CinderCityEffect(ElementalDamageBoostEffect):
+    """烬城勇者绘卷效果"""
+    def __init__(self, character,element_type):
+        super().__init__(character, '烬城勇者绘卷', element_type, 12, 12*60)
+        self.stacks = {}
+        self.nightsoul_stacks = {}
+        self.nightsoul_duration = 20*60
+        self.nightsoul_bonus = 28
+
+    def apply(self,element_type):
+        # 防止重复应用
+        for character in Team.team:
+            existing = next((e for e in character.active_effects 
+                        if isinstance(e, CinderCityEffect) and e.name == self.name), None)
+            if existing:
+                for i in element_type:
+                    if i in existing.stacks.keys():
+                        existing.stacks[i] = self.duration
+                    else:
+                        existing.apply_element(i)
+                    if self.character.Nightsoul_Blessing:
+                        if i in existing.nightsoul_stacks.keys():
+                            existing.nightsoul_stacks[i] = self.nightsoul_bonus
+                        else:
+                            existing.apply_nightsoul(i)
+                return
+            for element in self.element_type:
+                character.attributePanel[element+'元素伤害加成'] += self.bonus
+                character.add_effect(self)
+                self.stacks[element] = self.duration
+                if self.character.Nightsoul_Blessing:
+                    character.attributePanel[element+'元素伤害加成'] += self.nightsoul_bonus
+                    self.nightsoul_stacks[element] = self.nightsoul_bonus
+            print(f"{character.name}获得{element_type}烬城勇者绘卷效果")
+
+    def apply_element(self,element):
+        for character in Team.team:
+            character.attributePanel[element+'元素伤害加成'] += self.bonus
+            self.stacks[element] = self.duration
+
+    def apply_nightsoul(self,element):
+        for character in Team.team:
+            character.attributePanel[element+'元素伤害加成'] += self.nightsoul_bonus
+            self.nightsoul_stacks[element] = self.nightsoul_bonus
+        
+    def remove(self):
+        for character in Team.team:
+            for element in self.element_type:
+                character.attributePanel[element+'元素伤害加成'] -= self.bonus
+            character.remove_effect(self)
+            print(f"{character.name}: {self.name}效果结束")
+
+    def remove_element(self,element):
+        for character in Team.team:
+            character.attributePanel[element+'元素伤害加成'] -= self.bonus
+        del self.stacks[element]
+
+    def remove_nightsoul(self,element):
+        for character in Team.team:
+            character.attributePanel[element+'元素伤害加成'] -= self.nightsoul_bonus
+        del self.nightsoul_stacks[element]
+
+    def update(self):
+        for elemment,time in self.stacks.items():
+            self.stacks[elemment] -= 1
+            if self.stacks[elemment] <= 0:
+                self.remove_element(elemment)
+        for elemment,time in self.nightsoul_stacks.items():
+            self.nightsoul_stacks[elemment] -= 1
+            if self.nightsoul_stacks[elemment] <= 0:
+                self.remove_nightsoul(elemment)
+        if sum(self.nightsoul_stacks.values()) <= 0 and sum(self.stacks.values()) <= 0:
+            self.remove()
 
 class ScrolloftheHeroOfCinderCity(ArtifactEffect):
     def __init__(self):
