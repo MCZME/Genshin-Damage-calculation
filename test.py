7a52920aa46b9674b44adcb5c354a5cba64924e5
MCZME <126310378+MCZME@users.noreply.github.com>
2025-04-15 20:36:18 +0800

    添加元素反应测试

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from main import init
-from tests.elementalAura_test import test
+from tests.elementalAura_test import test_ELECTRO_CHARGED, test_SWIRL
 
 if __name__ == '__main__':
     init()
-    test()
+    test_SWIRL()
--- tests/elementalAura_test.py
+++ tests/elementalAura_test.py
@@ -3,7 +3,7 @@ from setup.Calculation.DamageCalculation import Damage, DamageType
 from setup.Target import Target
 
 
-def test():
+def test_ELECTRO_CHARGED():
     print('test')
     c = Character()
     target = Target(0,103)
@@ -23,4 +23,26 @@ def test():
             target.apply_elemental_aura(damage)
             damage.element = ('雷',1)
             target.apply_elemental_aura(damage)
+    print(target.elementalAura)
+
+def test_SWIRL():
+    print('test')
+    c = Character()
+    target = Target(0,103)
+    damage = Damage(0,('水',1),DamageType.NORMAL,'测试')
+    damage.setSource(c)
+    damage.setTarget(target)
+    target.apply_elemental_aura(damage)
+    damage.element = ('雷',1)
+    target.apply_elemental_aura(damage)
+
+    print(target.elementalAura)
+    for i in range(200):
+        target.update()
+        if i == 60:
+            damage.element = ('风',2)
+            target.apply_elemental_aura(damage)
+            
+
+
     print(target.elementalAura)
