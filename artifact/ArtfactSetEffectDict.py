a811428bc5b82ef71da78c3a68dc46d3cda48cc5
MCZME <126310378+MCZME@users.noreply.github.com>
2025-05-09 03:40:33 +0800

    重构 ArtfactSetEffectDict.py

--- artifact/ArtfactSetEffectDict.py
+++ artifact/ArtfactSetEffectDict.py
@@ -2,16 +2,7 @@ from artifact.ArtifactSetEffect import *
 
 
 ArtfactSetEffectDict = {
-    '角斗士的终幕礼': GladiatorFinale(),
-    '黑曜秘典': ObsidianCodex(),
-    '烬城勇者绘卷': ScrolloftheHeroOfCinderCity(),
-    '绝缘之旗印': EmblemOfSeveredFate(),
-    '昔时之歌': SongOfDaysPast(),
-    '教官': Instructor(),
-    '昔日宗室之仪': NoblesseOblige(),
-    '黄金剧团': GoldenTroupe(),
-    '逐影猎人': MarechausseeHunter(),
-    '翠绿之影':ViridescentVenerer(),
-    '深林的记忆':DeepwoodMemories(),
-    '乐园遗落之花':FlowerOfParadiseLost(),
+    cls().name: cls()
+    for name, cls in globals().items()
+    if isinstance(cls, type) and isinstance(cls(), ArtifactEffect)
 }
