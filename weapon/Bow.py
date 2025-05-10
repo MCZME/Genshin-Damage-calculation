355faa38a55a230a7154ba74b3d5e1ba9388cee9
MCZME <126310378+MCZME@users.noreply.github.com>
2025-05-11 03:06:21 +0800

    feat(weapon): 添加若水武器及其效果

--- weapon/Bow.py
+++ weapon/Bow.py
@@ -25,6 +25,19 @@ class SacrificialBow(Weapon,EventHandler):
                         self.character.Skill.cd_timer = self.character.Skill.cd
                         get_emulation_logger().log_skill_use("⌚" + self.character.name + f' 触发{self.name}')
 
+class AquaSimulacra(Weapon):
+    ID = 103
+    def __init__(self, character, level=1, lv=1):
+        super().__init__(character, AquaSimulacra.ID, level, lv)
+        self.hp_bonus = [16,20,24,28,32]
+        self.dmg_bonus = [20,25,30,35,40]
+
+    def skill(self):
+        # 默认周围存在敌人
+        self.character.attributePanel['生命值%'] += self.hp_bonus[self.lv - 1]
+        self.character.attributePanel['伤害加成'] += self.dmg_bonus[self.lv - 1]
+
 bow = {
+    '若水':AquaSimulacra,
     '祭礼弓':SacrificialBow,
 }
